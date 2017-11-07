5de94af734769bc83279eee7b3f564d0c38618e9
Pierre Neidhardt <ambrevar@gmail.com>
2017-11-07 10:53:13 +0100

    ztree: Wrap in setup function

--- evil-ztree.el
+++ evil-ztree.el
@@ -24,40 +24,41 @@
 ;; see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
+;; Evil bindings for ztree.
 
 ;;; Code:
-
 (require 'evil)
 (require 'ztree)
 
-(evil-set-initial-state 'ztree-mode 'motion)
+(defun evil-ztree-setup ()
+  (evil-set-initial-state 'ztree-mode 'motion)
 
-(evil-define-key 'motion ztree-mode-map
-  (kbd "<tab>") 'ztree-jump-side
-  (kbd "<return>") 'ztree-perform-action
-  (kbd "SPC") 'ztree-perform-soft-action
+  (evil-define-key 'motion ztree-mode-map
+    (kbd "<tab>") 'ztree-jump-side
+    (kbd "<return>") 'ztree-perform-action
+    (kbd "SPC") 'ztree-perform-soft-action
 
-  "x" 'ztree-toggle-expand-subtree
+    "x" 'ztree-toggle-expand-subtree
 
-  ;; update
-  "gr" 'ztree-refresh-buffer
+    ;; update
+    "gr" 'ztree-refresh-buffer
 
-  ;; quit
-  "q" 'quit-window
-  "ZQ" 'quit-window
-  "ZZ" 'quit-window)
+    ;; quit
+    "q" 'quit-window
+    "ZQ" 'quit-window
+    "ZZ" 'quit-window)
 
-(evil-define-minor-mode-key 'motion 'ztreediff-mode
-  "C" 'ztree-diff-copy
-  "D" 'ztree-diff-delete-file
-  "zH" 'ztree-diff-toggle-show-filtered-files
-  "d" 'ztree-diff-simple-diff-files
-  "zh" 'ztree-diff-toggle-show-equal-files
-  "gf" 'ztree-diff-view-file
+  (evil-define-minor-mode-key 'motion 'ztreediff-mode
+    "C" 'ztree-diff-copy
+    "D" 'ztree-diff-delete-file
+    "zH" 'ztree-diff-toggle-show-filtered-files
+    "d" 'ztree-diff-simple-diff-files
+    "zh" 'ztree-diff-toggle-show-equal-files
+    "gf" 'ztree-diff-view-file
 
-  ;; update
-  "gr" 'ztree-diff-partial-rescan
-  "gR" 'ztree-diff-full-rescan)
+    ;; update
+    "gr" 'ztree-diff-partial-rescan
+    "gR" 'ztree-diff-full-rescan))
 
-(provide 'init-evil-ztree)
+(provide 'evil-ztree)
 ;;; evil-ztree.el ends here
