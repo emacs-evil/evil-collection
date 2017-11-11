0ecdcb238d2dac454084a00bf179119236051807
James Nguyen <james@jojojames.com>
2017-11-11 13:26:51 -0800

    Edebug: Fix url

--- evil-edebug.el
+++ evil-edebug.el
@@ -5,7 +5,7 @@
 ;; Author: James Nguyen <james@jojojames.com>
 ;; Maintainer: James Nguyen <james@jojojames.com>
 ;; Pierre Neidhardt <ambrevar@gmail.com>
-;; URL: https://github.com/jojojames/evil-integrations
+;; URL: https://github.com/jojojames/evil-collection
 ;; Version: 0.0.1
 ;; Package-Requires: ((emacs "25.1"))
 ;; Keywords: evil, edebug, tools
