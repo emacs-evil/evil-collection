ff96b93199eadd98b51747b984d7311103380658
James Nguyen <james@jojojames.com>
2017-11-11 14:21:46 -0800

    Company: Add a few movement keys

--- evil-company.el
+++ evil-company.el
@@ -31,6 +31,22 @@
 (require 'evil)
 
 (defun evil-company-setup ()
+  (define-key company-active-map (kbd "C-j") #'company-select-next-or-abort)
+  (define-key company-active-map (kbd "C-k") #'company-select-previous-or-abort)
+  (define-key company-active-map (kbd "M-j") #'company-select-next)
+  (define-key company-active-map (kbd "M-k") #'company-select-previous)
+
+  (when evil-want-C-u-scroll
+    (define-key company-active-map (kbd "C-u") #'company-previous-page))
+
+  (when evil-want-C-d-scroll
+    (define-key company-active-map (kbd "C-d") #'company-next-page))
+
+  (define-key company-search-map (kbd "C-j") #'company-select-next-or-abort)
+  (define-key company-search-map (kbd "C-k") #'company-select-previous-or-abort)
+  (define-key company-search-map (kbd "M-j") #'company-select-next)
+  (define-key company-search-map (kbd "M-k") #'company-select-previous)
+
   ;; Sets up YCMD like behavior.
   (company-tng-configure-default))
 
