63f73367167f2eb0afead224f2484ef5ac299608
James Nguyen <james@jojojames.com>
2017-12-24 09:10:05 -0800

    IBuffer: Add l
    Since ibuffer is an overriding-map in evil-integration.el, we need to
    override these keys for now.
    
    Once we disable the evil-integration of evil, we can remove these
    extra keybindings.

--- evil-collection-ibuffer.el
+++ evil-collection-ibuffer.el
@@ -34,6 +34,7 @@
   "Set up `evil' bindings for `ibuffer'."
   (evil-set-initial-state 'ibuffer-mode 'normal)
   (evil-define-key 'normal ibuffer-mode-map
+    "l" 'evil-forward-char
     "gg" 'evil-goto-first-line
     (kbd "m") 'ibuffer-mark-forward
     (kbd "t") 'ibuffer-toggle-marks
