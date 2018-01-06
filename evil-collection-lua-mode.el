7a35806560b63cac6a3d5fe0d13fa74530919221
James Nguyen <james@jojojames.com>
2018-01-06 09:54:36 -0800

    Lua: Defvar and make byte compiler happy

--- evil-collection-lua-mode.el
+++ evil-collection-lua-mode.el
@@ -30,6 +30,8 @@
 (require 'evil)
 (require 'lua-mode nil t)
 
+(defvar lua-indent-level)
+
 (defun evil-collection-lua-mode-set-evil-shift-width ()
   "Set `evil-shift-width' according to `lua-indent-level'."
   (setq evil-shift-width lua-indent-level))
