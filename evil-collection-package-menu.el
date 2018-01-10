e3b8da1fa39eeec93513a2fae768f6cb222a82d7
James Nguyen <james@jojojames.com>
2018-01-09 19:31:43 -0800

    Package-menu: Use normal state

--- evil-collection-package-menu.el
+++ evil-collection-package-menu.el
@@ -32,9 +32,9 @@
 
 (defun evil-collection-package-menu-setup ()
   "Set up `evil' bindings for `package-menu'."
-  (evil-set-initial-state 'package-menu-mode 'motion)
+  (evil-set-initial-state 'package-menu-mode 'normal)
 
-  (evil-define-key 'motion package-menu-mode-map
+  (evil-define-key 'normal package-menu-mode-map
     "i" 'package-menu-mark-install
     "U" 'package-menu-mark-upgrades
     "d" 'package-menu-mark-delete
