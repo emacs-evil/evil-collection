1cf5f1b9e65be50954a2d8a10316624661720613
Pierre Neidhardt <ambrevar@gmail.com>
2018-01-16 11:29:47 +0100

    info: Fix missing require for evil-search

--- evil-collection-info.el
+++ evil-collection-info.el
@@ -31,6 +31,7 @@
 (require 'evil)
 (require 'evil-collection-evil-search)
 (require 'info)
+(require 'evil-collection-evil-search)
 
 (defun evil-collection-info-setup ()
   "Set up `evil' bindings for `info-mode'."
