d8230d215bcae15f2f1c98c1cdf0968770cc21eb
Maximiliano Sandoval <msandova@protonmail.com>
2018-01-26 22:29:12 -0300

    Added legal stuff.

--- evil-collection-reftex.el
+++ evil-collection-reftex.el
@@ -1,5 +1,7 @@
 ;;; evil-collection-reftex.el --- Bindings for `reftex'. -*- lexical-binding: t -*-
 
+;; Copyright (C) 2017 Pierre Neidhardt
+
 ;; Author: Maximiliano Sandoval <msandova@protonmail.com>
 ;; Maintainer: James Nguyen <james@jojojames.com>
 ;; Pierre Neidhardt <ambrevar@gmail.com>
@@ -8,11 +10,23 @@
 ;; Package-Requires: ((emacs "25.1"))
 ;; Keywords: evil, reftex, tools
 
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published
+;; by the Free Software Foundation; either version 3, or (at your
+;; option) any later version.
+;;
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 ;; Evil bindings for `reftex-mode'.
 
 ;;; Code:
-(eval-when-compile (require 'cl))
 (require 'evil)
 (require 'reftex-ref nil t)
 (require 'reftex-ref nil t)
