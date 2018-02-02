90c3293323155645ec459943216cec4326720332
Maximiliano Sandoval <msandova@protonmail.com>
2018-02-02 10:02:54 -0300

    Spaces

--- evil-collection-neotree.el
+++ evil-collection-neotree.el
@@ -38,60 +38,60 @@
 
   (evil-define-key 'normal neotree-mode-map
     
-    (kbd "<return>")	(neotree-make-executor
+    (kbd "<return>") (neotree-make-executor
 			 :file-fn 'neo-open-file
 			 :dir-fn 'neo-open-dir)
-    (kbd "<tab>")	(neotree-make-executor
+    (kbd "<tab>") (neotree-make-executor
 			 :dir-fn 'neo-open-dir)
-    "z"		(neotree-make-executor
-		 :dir-fn 'neo-open-dir)
-    "ZZ"	'quit-window
-    "gd"	(neotree-make-executor
-		 :dir-fn 'neo-open-dired)
-    "gD"	(neotree-make-executor
-		 :dir-fn 'neo-open-dired)
-    "go"	(neotree-make-executor
-		 :file-fn 'neo-open-file
-		 :dir-fn 'neo-open-dir)
-    "gO"	'neotree-quick-look
-    "gr"	'neotree-refresh
-    "q"		'neotree-hide
-    "H"		'neotree-hidden-file-toggle
-    "gh"	'neotree-hidden-file-toggle
-    (kbd "C-k")	'neotree-select-up-node
-    "gk"	'neotree-select-up-node
-    "["	'neotree-select-up-node
-    (kbd "C-j")	'neotree-select-down-node
-    "gj"	'neotree-select-down-node
-    "]"	'neotree-select-down-node
-    "gv"	'neotree-open-file-in-system-application
-    "c"		'neotree-create-node
-    "y"		'neotree-copy-node
-    "r"		'neotree-rename-node
-    "R"		'neotree-change-root
-    "d"		'neotree-delete-node
-    "J"		'neotree-dir
-    "+"		'neotree-stretch-toggle
-    "="		'neotree-stretch-toggle
-    "ge"	'neotree-enter
-    "j"		'neotree-next-line
-    "k"		'neotree-previous-line
+    "z" (neotree-make-executor
+ :dir-fn 'neo-open-dir)
+    "ZZ" 'quit-window
+    "gd" (neotree-make-executor
+ :dir-fn 'neo-open-dired)
+    "gD" (neotree-make-executor
+ :dir-fn 'neo-open-dired)
+    "go" (neotree-make-executor
+ :file-fn 'neo-open-file
+ :dir-fn 'neo-open-dir)
+    "gO" 'neotree-quick-look
+    "gr" 'neotree-refresh
+    "q" 'neotree-hide
+    "H" 'neotree-hidden-file-toggle
+    "gh" 'neotree-hidden-file-toggle
+    (kbd "C-k") 'neotree-select-up-node
+    "gk" 'neotree-select-up-node
+    "[" 'neotree-select-up-node
+    (kbd "C-j") 'neotree-select-down-node
+    "gj" 'neotree-select-down-node
+    "]" 'neotree-select-down-node
+    "gv" 'neotree-open-file-in-system-application
+    "c" 'neotree-create-node
+    "y" 'neotree-copy-node
+    "r" 'neotree-rename-node
+    "R" 'neotree-change-root
+    "d" 'neotree-delete-node
+    "J" 'neotree-dir
+    "+" 'neotree-stretch-toggle
+    "=" 'neotree-stretch-toggle
+    "ge" 'neotree-enter
+    "j" 'neotree-next-line
+    "k" 'neotree-previous-line
 
     ;; Unchanged keybings.
-    "a"		(neotree-make-executor
-		 :file-fn 'neo-open-file-ace-window)
-    "|"		(neotree-make-executor
-		 :file-fn 'neo-open-file-vertical-split)
-    "-"		(neotree-make-executor
-		 :file-fn 'neo-open-file-horizontal-split)
-    "S"		'neotree-select-previous-sibling-node
-    "s"		'neotree-select-next-sibling-node
-    (kbd "C-c C-c")	'neotree-change-root
-    (kbd "C-x 1")	'neotree-empty-fn
-    (kbd "C-x 2")	'neotree-empty-fn
-    (kbd "C-x 3")	'neotree-empty-fn
-    (kbd "C-x C-f")	'find-file-other-window
-    (kbd "C-c C-f")	'find-file-other-window))
+    "a" (neotree-make-executor
+ :file-fn 'neo-open-file-ace-window)
+    "|" (neotree-make-executor
+ :file-fn 'neo-open-file-vertical-split)
+    "-" (neotree-make-executor
+ :file-fn 'neo-open-file-horizontal-split)
+    "S" 'neotree-select-previous-sibling-node
+    "s" 'neotree-select-next-sibling-node
+    (kbd "C-c C-c") 'neotree-change-root
+    (kbd "C-x 1") 'neotree-empty-fn
+    (kbd "C-x 2") 'neotree-empty-fn
+    (kbd "C-x 3") 'neotree-empty-fn
+    (kbd "C-x C-f") 'find-file-other-window
+    (kbd "C-c C-f") 'find-file-other-window))
 
 (provide 'evil-collection-neotree)
 ;;; evil-collection-neotree.el ends here
