799866e90ec6630c559185111fd123badf0bb343
William Carroll <wpcarro@gmail.com>
2018-03-31 22:14:01 -0400

    Add licensing information
    Adds licensing copy, which was taken from another module in this repository.

--- evil-collection-git-timemachine.el
+++ evil-collection-git-timemachine.el
@@ -1,8 +1,24 @@
 ;;; evil-collection-git-timemachine.el --- Bindings for `git-timemachine' -*- lexical-binding: t -*-
+
 ;; Author: William Carroll <wpcarro@gmail.com>
+;; URL: https://github.com/emacs-evil/evil-collection
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-;; Evil keybindings for `git-timemachine' that conform to the principles outlines in evil-collection
+;; Evil keybindings for `git-timemachine' that conform to the principles
+;; outlines in evil-collection
 
 ;;; Code:
 (require 'evil)
