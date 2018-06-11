2b603d365cd2ce50e86c6db82c3d68965c964a65
Pierre Neidhardt <ambrevar@gmail.com>
2018-06-11 08:18:54 +0200

    Make it explicit that minibuffer-related packages (e.g. Helm) need the option

--- evil-collection.el
+++ evil-collection.el
@@ -30,6 +30,7 @@
 ;; If you want to use Evil in the minibuffer, you'll have to enable it by
 ;; setting `evil-collection-setup-minibuffer' to t before loading this package.
 ;; This is so because many users find it confusing.
+;; Some minibuffer-related packages such as Helm rely on this option.
 
 ;;; Code:
 (eval-when-compile (require 'subr-x))
