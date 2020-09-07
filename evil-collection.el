c621384823a7ab4d8dfd022ef00761e95b5854d2
Carla Cao <ccao001@gmail.com>
2020-09-07 07:03:45 -0400

    Add org-present

--- evil-collection.el
+++ evil-collection.el
@@ -211,6 +211,7 @@ through removing their entry from `evil-collection-mode-list'."
     ;; occur is in replace.el which was built-in before Emacs 26.
     (occur ,(if (<= emacs-major-version 25) "replace" 'replace))
     omnisharp
+    org-present
     osx-dictionary
     outline
     p4
