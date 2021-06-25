a8b0087084f7b3d05f5c0ef4612ff6de10edb4d8
Daanturo <daanturo@gmail.com>
2021-06-25 10:03:52 +0700

    Define tests for state filtering

--- test/evil-collection-test.el
+++ test/evil-collection-test.el
@@ -5,4 +5,37 @@
   "Zero check blank test."
   (should (equal 0 0)))
 
+(ert-deftest evil-collection-filtering-states-test ()
+  "Test `evil-collection--filter-states'."
+  (let ((evil-collection-state-denylist '())
+        (evil-collection-state-passlist '()))
+    (should
+     (equal nil
+            (evil-collection--filter-states nil)))
+    (should
+     (equal '(normal)
+            (evil-collection--filter-states 'normal)))
+    (should
+     (equal '(normal)
+            (evil-collection--filter-states '(normal)))))
+  (let ((evil-collection-state-denylist '(insert))
+        (evil-collection-state-passlist '()))
+    (should
+     (equal '()
+            (evil-collection--filter-states 'insert)))
+    (should
+     (equal '(visual)
+            (evil-collection--filter-states '(visual insert)))))
+  (let ((evil-collection-state-denylist '(insert))
+        (evil-collection-state-passlist '(normal visual)))
+    (should
+     (equal '()
+            (evil-collection--filter-states '())))
+    (should
+     (equal '(visual)
+            (evil-collection--filter-states '(insert visual))))
+    (should
+     (seq-set-equal-p '(visual normal)
+                      (evil-collection--filter-states '(motion normal visual insert))))))
+
 ;;; evil-collection-test.el ends here
