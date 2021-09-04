869d056dcab5eb9781824bcd591bf946969b8b41
condy <condy0919@gmail.com>
2021-09-04 16:29:50 +0800

    dictionary: remove redundant evil bindings

--- modes/dictionary/evil-collection-dictionary.el
+++ modes/dictionary/evil-collection-dictionary.el
@@ -38,11 +38,9 @@
   (evil-set-initial-state 'dictionary-mode 'normal)
   (evil-collection-set-readonly-bindings 'dictionary-mode-map)
   (evil-collection-define-key 'normal 'dictionary-mode-map
-    ;; motion
-    (kbd "l") 'evil-forward-char  ; otherwise bound to `dictionary-previous'
-    (kbd "h") 'evil-backward-char ; otherwise bound to `dictionary-help'
-    (kbd "p") 'ignore             ; otherwise bound to `backward-button'
-    (kbd "n") 'evil-search-next   ; otherwise bound to `forward-button'
+    ;; button
+    [tab] 'forward-button
+    [backtab] 'backward-button
     ;; mouse
     [mouse-1] 'link-selected
     ;; misc
