4edcbf4cc232d4c5e9f044bff6fa1b0813b116ce
James Nguyen <james@jojojames.com>
2022-04-14 11:05:32 -0400

    Add keyword for vundo

--- modes/vundo/evil-collection-vundo.el
+++ modes/vundo/evil-collection-vundo.el
@@ -7,7 +7,7 @@
 ;; URL: https://github.com/emacs-evil/evil-collection
 ;; Version: 0.0.1
 ;; Package-Requires: ((emacs "27.1"))
-;; Keywords: evil, vundo
+;; Keywords: evil, vundo, convenience
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
