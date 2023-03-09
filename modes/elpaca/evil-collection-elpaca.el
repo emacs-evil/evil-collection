88e5ce8bdb0762119b4153ed3346b486029f26d3
James Nguyen <james@jojojames.com>
2023-03-09 00:28:05 -0500

    Tweak

--- modes/elpaca/evil-collection-elpaca.el
+++ modes/elpaca/evil-collection-elpaca.el
@@ -23,7 +23,7 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-;;; Bindings for elpaca.
+;;; Bindings for `elpaca'.
 
 ;;; Code:
 (require 'evil-collection)
