5d26b55d0a714837fe18aa9ea40df1d03d3bdcfe
James Nguyen <james@jojojames.com>
2023-03-11 16:21:05 -0500

    Bind elpaca-ui-visit

--- modes/elpaca/evil-collection-elpaca.el
+++ modes/elpaca/evil-collection-elpaca.el
@@ -92,12 +92,15 @@ If this is nil, match original `elpaca' behavior."
       (kbd "U") 'elpaca-ui-unmark
       (kbd "u") 'elpaca-ui-mark-update))
 
-  (if evil-collection-elpaca-want-v
+  (let ((elpaca-visit-fn (if (fboundp 'elpaca-ui-visit)
+                             'elpaca-ui-visit
+                           'elpaca-visit)))
+    (if evil-collection-elpaca-want-v
+        (evil-collection-define-key 'normal 'elpaca-ui-mode-map
+          (kbd "gv") elpaca-visit-fn
+          (kbd "gd") elpaca-visit-fn)
       (evil-collection-define-key 'normal 'elpaca-ui-mode-map
-        (kbd "gv") 'elpaca-visit
-        (kbd "gd") 'elpaca-visit)
-    (evil-collection-define-key 'normal 'elpaca-ui-mode-map
-      (kbd "v") 'elpaca-visit))
+        (kbd "v") elpaca-visit-fn)))
 
   (if evil-collection-elpaca-want-g-filters
       (evil-collection-define-key 'normal 'elpaca-ui-mode-map
