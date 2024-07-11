4f008659d992b62040473346079683ee34edbafd
Julian Hoch <julian.hoch@hoch-software.ch>
2024-07-11 15:52:58 +0200

    Added hyrolo search result bindings

---
+++ modes/hyrolo/evil-collection-hyrolo.el
@@ -0,0 +1,27 @@
+;;; evil-collection-hyrolo.el --- Bindings for `hyrolo' -*- lexical-binding: t -*-
+
+;;; Commentary:
+;;; Bindings for hyrolo.
+
+;;; Code:
+(require 'evil-collection)
+(require 'hyperbole nil t)
+
+(defvar eldoc-mode-map)
+(defconst evil-collection-eldoc-maps '(hyrolo-mode-map))
+
+(defun evil-collection-hyrolo-setup ()
+  "Set up `evil' bindings for hyrolo."
+  (evil-collection-define-key 'normal 'hyrolo-mode-map
+    (kbd "[[") #'hyrolo-to-previous-loc
+    (kbd "]]") #'hyrolo-to-next-loc
+    (kbd "C-j") #'hyrolo-to-previous-loc
+    (kbd "C-k") #'hyrolo-to-next-loc
+    (kbd "gj") #'hyrolo-to-previous-loc
+    (kbd "gk") #'hyrolo-to-next-loc
+    (kbd "zo") #'hyrolo-outline-show-all
+    (kbd "zc") #'hyrolo-overview
+    ))
+
+(provide 'evil-collection-hyrolo)
+;;; evil-collection-hyrolo.el ends here
