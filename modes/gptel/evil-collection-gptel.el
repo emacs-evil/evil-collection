cdfe3514bc742dd0d5600b1b3409e49bac39337c
James Nguyen <james@jojojames.com>
2024-11-23 14:12:20 -0500

    Remove keywords

--- modes/gptel/evil-collection-gptel.el
+++ modes/gptel/evil-collection-gptel.el
@@ -7,7 +7,7 @@
 ;; URL: https://github.com/emacs-evil/evil-collection
 ;; Version: 0.0.1
 ;; Package-Requires: ((emacs "26.3"))
-;; Keywords: evil, emacs, tools
+;; Keywords: emacs
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
