602ccef0e521c9e1798097ddc2fa95a0bdf28964
0WD0 <wd.1105848296@gmail.com>
2026-06-23 06:11:26 +0800

    Add header for fj

--- modes/fj/evil-collection-fj.el
+++ modes/fj/evil-collection-fj.el
@@ -1,4 +1,26 @@
-;;; evil-collection-fj.el --- Evil bindings for fj -*- lexical-binding: t; -*-
+;;; evil-collection-fj.el --- Evil bindings for fj -*- lexical-binding: t -*-
+
+;; Copyright (C) 2026 0WD0
+
+;; Author: 0WD0 <wd.1105848296@gmail.com>
+;; Maintainer: James Nguyen <james@jojojames.com>
+;; URL: https://github.com/emacs-evil/evil-collection
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "29.1"))
+;; Keywords: evil, fj, tools
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;; Evil bindings for fj.el.
